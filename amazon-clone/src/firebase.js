ea417c93e18870172c19e5eb59a7f99b3ae50eba
kcbar <Kbarter@mail.greenriver.edu>
2020-09-17 17:10:07 -0700

    Blank page and took out unnescary React starter template code

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAmUr0isKzOVavne2gXkZzKsRBAPiFqYEM",
+    authDomain: "clone-d398e.firebaseapp.com",
+    databaseURL: "https://clone-d398e.firebaseio.com",
+    projectId: "clone-d398e",
+    storageBucket: "clone-d398e.appspot.com",
+    messagingSenderId: "1085003373062",
+    appId: "1:1085003373062:web:7cd6a3304aad0371c48187",
+    measurementId: "G-NCFWSCF8HE"
+  };
