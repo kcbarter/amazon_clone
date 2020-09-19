062325e5b0cbf2654beda980f1b963f362d30d9d
kcbar <Kbarter@mail.greenriver.edu>
2020-09-18 18:52:36 -0700

    Started working on header clone of Amazon

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './component/Header.js';
 
 function App() {
   return (
     <div className="app">
       <h1>Amazon Clone</h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
---
+++ amazon-clone/src/component/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../styling/header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className='header_logo'
+            src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+
+            <div className="header_search">
+                <input className="header_searchIn"/>
+                {/*Logo */}
+            </div>
+            <div className="header_nav">
+                <div className='header_option'>
+                    <span className='header__optionOne'>
+                        Hello
+                    </span>
+                    <span className='header__optionTwo'>
+                        Sign In
+                    </span>
+                </div>
+                <div className='header_option'>
+                <span className='header__optionOne'>
+                        Returns
+                    </span>
+                    <span className='header__optionTwo'>
+                        & Orders
+                    </span>
+                </div>
+                <div className='header_option'>
+                <span className='header__optionOne'>
+                        Your
+                    </span>
+                    <span className='header__optionTwo'>
+                        Prime
+                    </span>
+                </div>
+                <div className='header_option'>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
