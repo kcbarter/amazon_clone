a3f0a81e83f904203ae7168f2910bc8689213928
kcbar <Kbarter@mail.greenriver.edu>
2020-09-20 21:40:11 -0700

    Starting on the Home component

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './component/Header.js';
+import Home from './component/Home.js';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
